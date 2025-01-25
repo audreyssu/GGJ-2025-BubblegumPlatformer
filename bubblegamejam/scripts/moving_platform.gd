3a04c2e95719766df4be1de4d011475231fdff49
AudreySSU <deberryc@mymail.shawnee.edu>
2025-01-24 22:23:54 -0500

    Pendulum added

--- bubblegamejam/scripts/moving_platform.gd
+++ bubblegamejam/scripts/moving_platform.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$platformAnimPlayer.speed_scale *= moveSpeedScale
+	$pendulumAnimPlayer.speed_scale *= moveSpeedScale
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
