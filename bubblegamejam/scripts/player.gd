7d420a40e75b575b2180f95550793995d0f85217
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2025-01-24 19:41:40 -0500

    Basic player movement and camera

--- bubblegamejam/scripts/player.gd
+++ bubblegamejam/scripts/player.gd
@@ -1,28 +1,39 @@
 extends CharacterBody3D
 
+const MAX_BUBBLES:int = 3
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var moveSpeed:float = 5.0
+@export var jumpPower:float = 4.5
+
+@export var numBubbles:int = MAX_BUBBLES
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	#Blow bubbles
+	if Input.is_action_just_pressed("p_blowbubble") and numBubbles > 0:
+		numBubbles -= 1
+	
+	#for testing UI
+	if Input.is_action_just_pressed("p_blowbubble") and numBubbles <= 0:
+		numBubbles = MAX_BUBBLES
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("p_jump") and is_on_floor():
+		velocity.y = jumpPower
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("p_left", "p_right", "p_foreward", "p_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * moveSpeed
+		velocity.z = direction.z * moveSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, moveSpeed)
+		velocity.z = move_toward(velocity.z, 0, moveSpeed)
 
 	move_and_slide()
