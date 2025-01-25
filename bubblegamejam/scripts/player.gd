ea4cac35edeea0f57bc5076e6986596dc7328f52
TheJohnDarksoul <156362113+TheJohnDarksoul@users.noreply.github.com>
2025-01-24 21:52:14 -0500

    Camera controls are working

--- bubblegamejam/scripts/player.gd
+++ bubblegamejam/scripts/player.gd
@@ -7,28 +7,52 @@ const MAX_BUBBLES:int = 3
 
 @export var numBubbles:int = MAX_BUBBLES
 
+func _init() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
+
+func _input(event: InputEvent) -> void:
+	#Rotates the camera using the mouse
+	if event is InputEventMouseMotion:
+		#Found out I had to seperate the rotation across two nodes from the link below
+		#https://godotforums.org/d/24512-how-do-a-rotate-an-object-around-a-point/5
+		$h.rotate_y(deg_to_rad(-event.screen_relative.x * Settings.sensitivity))
+		$h/v.rotate_x(deg_to_rad(-event.relative.y * Settings.sensitivity))
+		$h/v.rotation.x = clampf($h/v.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		pass
+
+#Rotates the camera using a controller
+func rotateCameraController():
+	var rotationDir = Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
+	
+	if rotationDir:
+		$h.rotate_y(deg_to_rad(-rotationDir.x * 3))
+		$h/v.rotate_x(deg_to_rad(-rotationDir.y * 3))
+		$h/v.rotation.x = clampf($h/v.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	pass
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	rotateCameraController()
+	
 	#Blow bubbles
 	if Input.is_action_just_pressed("p_blowbubble") and numBubbles > 0:
 		numBubbles -= 1
 	
 	#for testing UI
-	if Input.is_action_just_pressed("p_blowbubble") and numBubbles <= 0:
-		numBubbles = MAX_BUBBLES
+	#if Input.is_action_just_pressed("p_blowbubble") and numBubbles < 0:
+	#	numBubbles = MAX_BUBBLES
 
-	# Handle jump.
 	if Input.is_action_just_pressed("p_jump") and is_on_floor():
 		velocity.y = jumpPower
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("p_left", "p_right", "p_foreward", "p_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, $h.rotation.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * moveSpeed
 		velocity.z = direction.z * moveSpeed
