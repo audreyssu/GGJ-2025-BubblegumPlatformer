e5506e222322b8c16fc92601540a8032d8008e28
Anatoliy2104 <89733031+Anatoliy2104@users.noreply.github.com>
2025-01-26 11:06:10 -0500

    Revert "Merge branch 'Kyle'"
    This reverts commit bf5a0baae3a48e1dad3f5247472296e2ffb938a0, reversing
    changes made to 3d0340b585a04c206f658608415ea06365257ac5.

--- bubblegamejam/scripts/player.gd
+++ bubblegamejam/scripts/player.gd
@@ -1,119 +1,28 @@
 extends CharacterBody3D
 
-const MAX_BUBBLES:int = 3
 
-@export_category("Movement Values")
-@export var maxSpeed:float = 1.5
-var speed:float = 0.0
-@export var jumpPower:float = 4.5
-@export var acceleration:float = 0.5
-@export var airAcceleration:float = 0 #Need to implement this
-@export var friction:float = 0.1
-@export var airResistance:float = 0 #Need to implement this
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
-@export_category("Misc. Values")
-@export var numBubbles:int = MAX_BUBBLES
-var rotateSpeed:float = 20
-
-var jumpSound = preload("res://assets/sounds/Bounce!.mp3")
-var bubbleSound = preload("res://assets/sounds/Bubble_Spawn.mp3")
-
-var bubbleScene:PackedScene = preload("res://scenes/bubble.tscn")
-
-signal numBubblesChanged(value)
-
-func _init() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	pass
-
-func _input(event: InputEvent) -> void:
-	#Rotates the camera using the mouse
-	if event is InputEventMouseMotion:
-		#Found out I had to seperate the rotation across two nodes from the link below
-		#https://godotforums.org/d/24512-how-do-a-rotate-an-object-around-a-point/5
-		$h.rotate_y(deg_to_rad(-event.screen_relative.x * Settings.sensitivity))
-		$h/v.rotate_x(deg_to_rad(-event.relative.y * Settings.sensitivity))
-		$h/v.rotation.x = clampf($h/v.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-		pass
-
-#Rotates the camera using a controller
-func rotateCameraController():
-	var rotationDir = Input.get_vector("cam_left", "cam_right", "cam_up", "cam_down")
-	
-	if rotationDir:
-		$h.rotate_y(deg_to_rad(-rotationDir.x * 3))
-		$h/v.rotate_x(deg_to_rad(-rotationDir.y * 3))
-		$h/v.rotation.x = clampf($h/v.rotation.x, deg_to_rad(-89), deg_to_rad(89))
-	pass
-	
-func jump(delta: float):
-	velocity.y = jumpPower
-	$playerSoundSource.stream = jumpSound
-	$playerSoundSource.play()
-	
-func blowBubble():
-	#Blow bubbles
-	if Input.is_action_just_pressed("p_blowbubble") and numBubbles > 0:
-		numBubbles -= 1
-		numBubblesChanged.emit(numBubbles)
-		
-		var newBubble = bubbleScene.instantiate()
-		get_tree().root.get_child(0).add_child(newBubble)
-		newBubble.global_position = $charMesh/bubbleSpawnPoint.global_position
-		#newBubble.set_scale(Vector3(0.02, 0.02, 0.02))
-		
-		$bubbleSoundSource.stream = bubbleSound
-		$bubbleSoundSource.play()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	rotateCameraController()
-	
-	blowBubble()
 
-	if Input.is_action_just_pressed("p_jump"):
-		if is_on_floor():
-			jump(delta)
-		else:
-			$jumpBufferTimer.start()
-			
-	#Jump buffering
-	if is_on_floor() and $jumpBufferTimer.get_time_left() > 0:
-		jump(delta)
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("p_left", "p_right", "p_foreward", "p_backward")
-	var direction:Vector3 = (Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, $h.rotation.y)).normalized()
-	
-	var yVel:float = velocity.y
-	velocity.y = 0
-	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = lerp(velocity.x, direction.x * maxSpeed, acceleration)
-		velocity.z = lerp(velocity.z, direction.z * maxSpeed, acceleration)
-		
-		if is_on_floor():
-			var curRot = Quaternion($charMesh.transform.basis)
-			$charMesh.look_at(global_position + direction, Vector3.UP)
-			var targRot = Quaternion($charMesh.transform.basis)
-			$charMesh.transform.basis = Basis(curRot.slerp(targRot, 0.3))
-			
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = lerp(velocity.x, 0.0, friction)
-		velocity.z = lerp(velocity.z, 0.0, friction)
-
-	velocity.y = yVel
-	#print(velocity)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-
-func collectGum():
-	if !(numBubbles + 1 > MAX_BUBBLES):
-		numBubbles += 1
-		numBubblesChanged.emit(numBubbles)
-		return true
-	return false
--- bubblegamejam/scripts/settings.gd
+++
@@ -1,4 +0,0 @@
-#Put all configuration variables in here
-extends Node
-
-var sensitivity:float = 0.31
